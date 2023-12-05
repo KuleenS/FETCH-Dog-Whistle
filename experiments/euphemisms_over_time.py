2b99ed8d596c1852827ceb5022fd76ad241e2b73
Kuleen Sasse <cometku@gmail.com>
2023-12-05 09:57:47 -0500

    Added another failsafe

--- experiments/euphemisms_over_time.py
+++ experiments/euphemisms_over_time.py
@@ -77,7 +77,11 @@ def main(args):
 
                 with db.stream(match_event_handler=on_match) as stream:
 
-                    for tweet in tqdm(tweets, desc=f"Processing {tweet_file}"):
+                    while True:
+                        try:
+                            tweet = next(tweets)
+                        except (IOError, StopIteration, zlib.error):
+                            break
 
                         tweet = tweet.strip()
 
