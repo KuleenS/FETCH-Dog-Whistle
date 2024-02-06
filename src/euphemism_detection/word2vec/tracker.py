e377b9bb14392ef6879fc754af530875f573ccad
Kuleen Sasse <cometku@gmail.com>
2024-02-05 23:28:01 -0500

    Removed imports from tracker

--- src/euphemism_detection/word2vec/tracker.py
+++ src/euphemism_detection/word2vec/tracker.py
@@ -54,15 +54,36 @@ import string
 from tqdm import tqdm
 
 ## Project Tools
-from mhlib.util.helpers import flatten, chunks
-from mhlib.util.logging import initialize_logger
+def flatten(l):
+    """
+    Flatten a list of lists by one level.
 
+    Args:
+        l (list of lists): List of lists
+    
+    Returns:
+        flattened_list (list): Flattened list
+    """
+    flattened_list = [item for sublist in l for item in sublist]
+    return flattened_list
+
+def chunks(l,
+           n):
+    """
+    Yield successive n-sized chunks from l.
+
+    Args:
+        l (list): List of objects
+        n (int): Chunksize
+    
+    Yields:
+        Chunks
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 ######################
 ### Globals
 ######################
-
-## Logger
-LOGGER = initialize_logger()
 
 ## Special Characters
 PUNCTUATION = string.punctuation.replace("#","")
