e6d8e56e56d992e33e6c0fc082737d2dbbf27da5
Kuleen Sasse <cometku@gmail.com>
2024-04-08 23:07:54 -0400

    Diversity debugging

--- src/euphemism_detection/fetch/rerank/diversity.py
+++ src/euphemism_detection/fetch/rerank/diversity.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from sklearn.cluster import KMeans
 
@@ -11,21 +11,27 @@ class DiversityReranker:
         self.clusters = clusters
 
     
-    def rerank(self, returned_embeddings: List[List[float]], top_k: int):
-        num_clusters = min(len(returned_embeddings),self.clusters)
-        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(returned_embeddings)
+    def rerank(self, docu_embeds: List[Tuple[str, List[float]]], top_k: int):
+        
+        embeddings = [x[1] for x in docu_embeds]
+        
+        documents = [x[0] for x in docu_embeds]
+        
+        num_clusters = min(len(embeddings),self.clusters)
+        
+        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(embeddings)
 
-        proportions = dict(zip(range(len(num_clusters)), [0]*len(num_clusters)))
+        proportions = dict(zip(range(num_clusters), [0]*num_clusters))
 
         for label in kmeans.labels_:
             proportions[label] += 1
         
         cluster_counts = {}
         for k,v in proportions.items():
-            cluster_counts[k] = math.ceil((v/len(returned_embeddings)) * top_k)
+            cluster_counts[k] = math.ceil((v/len(embeddings)) * top_k)
         
         cluster_docs = []
-        for i,doc in enumerate(returned_embeddings):
+        for i,doc in enumerate(documents):
             label = kmeans.labels_[i]
             if cluster_counts[label] > 0:
                 cluster_counts[label] -= 1
