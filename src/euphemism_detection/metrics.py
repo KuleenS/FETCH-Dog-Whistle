dd187af926ca90535c6b29cc523fd0bf39ace72e
Kuleen Sasse <cometku@gmail.com>
2024-04-08 23:07:46 -0400

    Updated metrics

--- src/euphemism_detection/metrics.py
+++ src/euphemism_detection/metrics.py
@@ -18,43 +18,52 @@ class Metrics:
         self.dogwhistle_file_path = dogwhistle_file_path
 
         dogwhistles_df = pd.read_csv(dogwhistle_file_path, sep="\t")
+
         dogwhistle_set = dogwhistles_df["Surface Forms"].str.split(";").tolist()
-        comparison_set = dogwhistles_df["Dogwhistle"].tolist()
+
+        comparison_set = [x.strip().lower() for x in dogwhistles_df["Dogwhistle"].tolist()]
         
         self.dogwhistle_to_surface = defaultdict(list)
 
         for i in range(len(dogwhistle_set)):
-            self.dogwhistle_to_surface[comparison_set[i]] = [x.strip().lower() for x in dogwhistle_set[i]]
+            self.dogwhistle_to_surface[comparison_set[i]] = set([x.strip().lower() for x in dogwhistle_set[i]] + [comparison_set[i]])
 
-        self.ngrams = {x.strip().lower() : min([len(word_tokenize(y)) for y in self.dogwhistle_to_surface[x]]) for x in comparison_set}
+        self.ngrams = {x : min([len(word_tokenize(y)) for y in self.dogwhistle_to_surface[x]]) for x in comparison_set}
 
         self.dogwhistles = dict()
 
         for i in range(len(dogwhistle_set)):
             for surface_form in dogwhistle_set[i]:
-                self.dogwhistles[surface_form.strip().lower()] = comparison_set[i].strip().lower()
+                self.dogwhistles[surface_form.strip().lower()] = comparison_set[i]
+            
+            self.dogwhistles[comparison_set[i]] = comparison_set[i]
+
         
     def measure_precision(self, predicted_dogwhistles: List[str], gold_dogwhistles: List[str]) -> float:
-        dogwhistle_map = {x: self.dogwhistles[x] for x in gold_dogwhistles}
+        dogwhistle_map = {x.strip().lower(): self.dogwhistles[x.strip().lower()] for x in gold_dogwhistles}
+
+        predicted_dogwhistles_cleaned = [x.strip().lower() for x in predicted_dogwhistles]
 
         dogwhistles_found = []
 
-        for dogwhistle in predicted_dogwhistles:
+        for dogwhistle in predicted_dogwhistles_cleaned:
             if dogwhistle in dogwhistle_map:
                 dogwhistles_found.append(dogwhistle_map[dogwhistle])
         
         if len(predicted_dogwhistles) == 0:
             return 0
 
-        return len(set(dogwhistles_found))/len(predicted_dogwhistles)
+        return len(dogwhistles_found)/len(predicted_dogwhistles_cleaned)
 
 
     def measure_recall(self, predicted_dogwhistles: List[str], gold_dogwhistles: List[str]) -> float:
         dogwhistle_map = {x: self.dogwhistles[x] for x in gold_dogwhistles}
+        
+        predicted_dogwhistles_cleaned = [x.strip().lower() for x in predicted_dogwhistles]
 
         dogwhistles_found = []
 
-        for dogwhistle in predicted_dogwhistles:
+        for dogwhistle in predicted_dogwhistles_cleaned:
             if dogwhistle in dogwhistle_map:
                 dogwhistles_found.append(dogwhistle_map[dogwhistle])
 
@@ -62,12 +71,14 @@ class Metrics:
 
     def measure_possible_recall(self, predicted_dogwhistles: List[str], gold_dogwhistles: List[str], max_possible_ngrams: int) -> float:
         dogwhistle_map = {x: self.dogwhistles[x] for x in gold_dogwhistles}
+        
+        predicted_dogwhistles_cleaned = [x.strip().lower() for x in predicted_dogwhistles]
 
         possible = [x for x in set(dogwhistle_map.values()) if self.ngrams[x] <= max_possible_ngrams]
-
+        
         dogwhistles_found = []
 
-        for dogwhistle in predicted_dogwhistles:
+        for dogwhistle in predicted_dogwhistles_cleaned:
             if dogwhistle in dogwhistle_map:
                 dogwhistles_found.append(dogwhistle_map[dogwhistle])
 
