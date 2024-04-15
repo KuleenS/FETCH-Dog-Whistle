443bbcc4e080a3cd932132ef74839e79364652a6
Kuleen Sasse <cometku@gmail.com>
2024-04-14 21:26:37 -0400

    modified run multiple to include eval

--- src/euphemism_detection/neural/run_multiple.py
+++ src/euphemism_detection/neural/run_multiple.py
@@ -4,6 +4,10 @@ import csv
 
 import os
 
+import pandas as pd
+
+from tqdm import tqdm
+
 from multiple_neural_euphemism import MultiNeuralEuphemismDetector
 
 from utils import DogwhistleSplitter
@@ -51,17 +55,45 @@ def main(args):
     euphemism_detector = MultiNeuralEuphemismDetector(given_dogwhistles_surface_forms, tweet_files, phrases, word2vec_path, args.data_name, args.output_path, args.model_name, args.threshold, True)
 
     euphemism_detector.run()
+
+    metrics = Metrics(os.path.join(args.dogwhistle_file_path, "glossary.tsv"))
+
+    files = [os.path.join(args.output_path, x) for x in os.listdir(args.output_path)]
     
-    # metrics = Metrics(os.path.join(args.dogwhistle_file_path, "glossary.tsv"))
+    predictions = []
 
-    # precision = metrics.measure_precision(top_words, extrapolating_dogwhistles_surface_forms)
+    for file in tqdm(files):
+        df = pd.read_csv(file)
+        
+        prediction = list(df["word"])[::-1]
+        
+        predictions.append(prediction)
+    
+    threholds = [50,
+    100,
+    200,
+    400,
+    800,
+    1600,
+    3200,
+    6400,
+    12800,
+    25600]
 
-    # recall = metrics.measure_recall(top_words, extrapolating_dogwhistles_surface_forms)
+    results = []
 
-    # possible_recall = metrics.measure_possible_recall(top_words, extrapolating_dogwhistles_surface_forms, 1)
+    for threshold in tqdm(threholds):
+        top_words = sum([x[:threshold] for x in predictions], [])
+        
+        precision = metrics.measure_precision(top_words, extrapolating_dogwhistles_surface_forms)
 
-    # print(precision, recall, possible_recall)
+        recall = metrics.measure_recall(top_words, extrapolating_dogwhistles_surface_forms)
 
+        possible_recall = metrics.measure_possible_recall(top_words, extrapolating_dogwhistles_surface_forms, 3)
+
+        results.append((threshold, precision, recall, possible_recall))
+    
+    print(results)
 
 
 
