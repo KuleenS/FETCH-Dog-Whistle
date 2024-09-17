bb1bd4618546ba82e7dbeef212f0a552a8e62c16
Kuleen Sasse <cometku@gmail.com>
2024-09-17 16:50:35 -0400

    modified predict to deal with everything

--- src/euphemism_detection/bert/predict.py
+++ src/euphemism_detection/bert/predict.py
@@ -8,12 +8,41 @@ class PredictBERT:
 
         self.model_folder = model_folder
 
-        self.classifier = pipeline( model=self.model_folder, tokenizer=self.model_folder, device=0)
+        self.max_length = {
+            "tomh/toxigen_hatebert": 510,
+            "GroNLP/hateBERT": 510,
+            "adediu25/subtle-toxicgenconprompt-all-no-lora": 510,
+            "adediu25/implicit-toxicgenconprompt-all-no-lora": 510,
+            "facebook/roberta-hate-speech-dynabench-r4-target": 512, 
+            "cardiffnlp/twitter-roberta-base-hate": 512,
+            "Hate-speech-CNERG/bert-base-uncased-hatexplain" :510,
+        }
+
+        if model_folder in self.max_length:
+            max_length = self.max_length[model_folder]
+        
+        else:
+            max_length = 510
+
+        if self.model_folder== "tomh/toxigen_hatebert":
+            self.classifier = pipeline("text-classification", model=self.model_folder, tokenizer="GroNLP/hateBERT", device="cuda:0", max_length = max_length, truncation=True)
+        elif self.model_folder in ["adediu25/subtle-toxicgenconprompt-all-no-lora", "adediu25/implicit-toxicgenconprompt-all-no-lora"]:
+            self.classifier = pipeline("text-classification", model=self.model_folder, tokenizer="youngggggg/ToxiGen-ConPrompt", device="cuda:0", max_length = max_length, truncation=True)
+        else:
+            self.classifier = pipeline("text-classification", self.model_folder, device="cuda:0", max_length = max_length, truncation=True)
     
     def prediction(self, X: List[str]) -> List[str]:
-        results = []
+        results = self.classifier(X)
 
-        for item in X:
-            results.append(self.classifier(item)["label"])
-        
+        if self.model_folder in ["tomh/toxigen_hatebert", "GroNLP/hateBERT"]:
+            results = [1 if x["label"] == "LABEL_1" else 0 for x in results]
+        elif self.model_folder in ["adediu25/subtle-toxicgenconprompt-all-no-lora", "adediu25/implicit-toxicgenconprompt-all-no-lora"]:
+            results = [0 if x["label"] == "Non-HS" else 1 for x in results]
+        elif self.model_folder in ["facebook/roberta-hate-speech-dynabench-r4-target", "cardiffnlp/twitter-roberta-base-hate-latest"]:
+            results = [1 if x["label"] == "hate" else 0 for x in results]
+        elif self.model_folder in ["Hate-speech-CNERG/bert-base-uncased-hatexplain"]:
+            results = [0 if x["label"] == "normal" else 1 for x in results]
+        elif self.model_folder in ["badmatr11x/distilroberta-base-offensive-hateful-speech-text-multiclassification"]:
+            results = [0 if x["label"] == "NEITHER" else 1 for x in results]
+
         return results
